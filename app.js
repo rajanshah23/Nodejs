1f878438a392d1c1bd1338871929a7dc6b1fa115
rajanshah23 <shahrajan774@gmail.com>
2025-06-22 23:36:43 +0545

    Register and login page

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express =require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-// We can also do like this 
+// We can also do like this
 // const app=require("express")()
 
-const PORT=3000
-app.use(express.json())
+const PORT = 3000;
+app.use(express.json());
 
 //templaing engine(Backend batai frontend render garna we use either(ejs,pug,handlebar,moustache))
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    const name="Rajan Gupta"
-    res.render('home.ejs',{data:name})
-    
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.get("/about",(req,res)=>{
-    res.render('about')
+app.get("/register", (req, res) => {
+  res.render("auth/register")
+});
 
-})
- app.listen( PORT,()=>{
-    console.log(`server has startd at http://localhost:${PORT}`);
-    
-})
- 
+app.get("/login", (req, res) => {
+  res.render("auth/login")
+});
+
+
+app.use(express.static("public/css/"))
+
+
+app.listen(PORT, () => {
+  console.log(`Server has startd at http://localhost:${PORT}`);
+});
